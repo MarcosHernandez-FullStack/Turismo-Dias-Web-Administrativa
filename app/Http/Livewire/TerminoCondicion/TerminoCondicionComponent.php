5968421630c55e4da067a69ec89cfa5a6fcea508
Elpredicador123 <agentepredicador@gmail.com>
2023-11-01 00:57:49 -0500

    limpieza componentes | 01/11/2023

--- app/Http/Livewire/TerminoCondicion/TerminoCondicionComponent.php
+++ app/Http/Livewire/TerminoCondicion/TerminoCondicionComponent.php
@@ -7,7 +7,6 @@ use Livewire\WithPagination;
 use Illuminate\Support\Facades\Storage;
 use Livewire\WithFileUploads;
 use App\Models\TerminoCondicion;
-use App\Models\Ciudad;
 use Illuminate\Database\Eloquent\Collection;
 
 class TerminoCondicionComponent extends Component
@@ -15,32 +14,25 @@ class TerminoCondicionComponent extends Component
 
     use WithPagination;
     use WithFileUploads;
-    public $termino_condicion,$ruta_foto_principal,$ruta_foto_secundaria,$beneficios_collection,$beneficio_id=-1,$foto_principal_guardada,$foto_secundaria_guardada;
+    public $termino_condicion;
     public $search, $sort, $direction;
     public $form, $vista;
     protected $paginationTheme = 'bootstrap';
     public $paginacion = 6;
-    /* public $paginacion, $paginationTheme; */
 
-    //CONSTRUCTOR EN DONDE SE INICIALIZAN VARIABLES
     public function mount()
     {
         $this->sort ='id';
         $this->direction ='asc';
-        $this->form = 'create'; //create, update
-        $this->vista = 'form'; //form
-       /*  $this->paginacion = 3;
-        $this->paginationTheme = 'bootstrap'; */
-        $this->beneficios_collection = new Collection();
+        $this->form = 'create';
+        $this->vista = 'form';
         $this->termino_condicion=new TerminoCondicion();
     }
 
-     //FUNCION PARA RESETEAR NUMERO DE PAGINACION
      public function updatingSearch(){
         $this->resetPage();
     }
 
-    //FUNCION PARA REGISTRAR LAS VALIDACIONES DINAMICAS
     protected function rules(){
         return [
            'termino_condicion.seccion' => 'required',
@@ -48,30 +40,24 @@ class TerminoCondicionComponent extends Component
         ];
    }
 
-   //PROPIEDAD PARA PERSONALIZAR MENSAJES DE VALIDACION
    protected $messages = [
             'termino_condicion.seccion.required' => 'La seccion es requerida',
             'termino_condicion.descripcion.required' => 'La descripcion es requerida',
    ];
 
-   //FUNCION PARA MOSTRAR ERRORES DE VALIDACION EN TIEMPO REAL
    public function updated($propertyName){
        $this->validateOnly($propertyName);
    }
 
-   //FUNCION PARA RESETEAR VARIABLES Y ERRORES DE VALIDACION
    public function resetError(){
        $this->resetErrorBag();
        $this->resetValidation();
    }
 
-   //FUNCION PARA MOSTRAR LA VISTA DEL MODAL
    public function showModal($vista, $form){
        $this->resetError();
        if($form == 'create'){
            $this->termino_condicion = new TerminoCondicion();
-           $this->reset('ruta_foto_principal','ruta_foto_secundaria');
-           $this->beneficios_collection = new Collection();
        }
        $this->vista = $vista;
        $this->form = $form;
@@ -80,35 +66,18 @@ class TerminoCondicionComponent extends Component
     public function render()
     {
         $termino_condiciones=TerminoCondicion::where('seccion', 'like', '%'.$this->search.'%')->paginate($this->paginacion);
-        $ciudades=Ciudad::where('estado','=','1')->get();
-        return view('livewire.termino_condicion.termino_condicion-component', compact('termino_condiciones','ciudades'))
+        return view('livewire.termino_condicion.termino_condicion-component', compact('termino_condiciones'))
                 ->extends('layouts.principal')
                 ->section('content');
     }
 
-
-    //FUNCION PARA GUARDAR EN BASE DE DATOS
     public function save(){
         $this->validate();
-
-        $servicioCreado= new TerminoCondicion();
-        $servicioCreado=$this->termino_condicion;
-        $servicioCreado->save();
+        $this->termino_condicion->save();
         session()->flash('message', 'TerminoCondicion registrado con éxito');
         $this->dispatchBrowserEvent('closeModal');
     }
 
-    public function imprimir()
-    {
-        dd($this->beneficios_collection->all());
-    }
-
-    //FUNCION PARA REDIRIGIR AL SUBSERVICIO
-    public function rediregirProyectos($servicio_id){
-        return redirect()->route('proyectos', ['servicio_id' => $servicio_id]);
-    }
-
-    //FUNCION PARA CAMBIAR EL ESTADO DEL MODELO
     public function cambiarEstado($id){
         $termino_condicion = TerminoCondicion::find($id);
         if($termino_condicion->estado == 1){
@@ -116,32 +85,15 @@ class TerminoCondicionComponent extends Component
         }else{
             $termino_condicion->update(['estado' => '1']);
         }
-        session()->flash('message', 'Estado del TerminoCondicion actualizado con éxito');    //ENVIAR MENSAJE DE CONFIRMACION
+        session()->flash('message', 'Estado del Termino Condicion actualizado con éxito');  
     }
 
-    //FUNCION PARA CONSULTAR EN BASE DE DATOS Y LLENAR LOS CAMPOS DEL FORMULARIO
     public function edit($id){
         $this->showModal("form", "update");
         $this->termino_condicion=TerminoCondicion::find($id);
-       /*  $this->ruta_foto_principal=$this->termino_condicion->ruta_foto_principal;
-        $this->ruta_foto_secundaria=$this->termino_condicion->ruta_foto_secundaria; */
-        $this->foto_principal_guardada = $this->termino_condicion->ruta_foto_principal;
-        $this->foto_secundaria_guardada = $this->termino_condicion->ruta_foto_secundaria;
-        $this->beneficio_id=-1;
-       /*  $this->reset('ruta_foto_principal','ruta_foto_secundaria'); */
     }
 
     public function update(){
-        /* if($this->termino_condicion->) */
-       /*  if($this->ruta_foto_principal) 
-            $this->termino_condicion->ruta_foto_principal = $this->ruta_foto_principal->store('public/termino_condiciones/principal');
-        else
-            $this->termino_condicion->ruta_foto_principal=$this->termino_condicion->ruta_foto_principal;
-        if($this->ruta_foto_secundaria) 
-            $this->termino_condicion->ruta_foto_secundaria = $this->ruta_foto_secundaria->store('public/termino_condiciones/secundaria');
-        else
-            $this->termino_condicion->ruta_foto_secundaria=$this->termino_condicion->ruta_foto_secundaria; */
-
         $this->validate();
         $this->termino_condicion->update();
         session()->flash('message', 'TerminoCondicion actualizado con éxito');
